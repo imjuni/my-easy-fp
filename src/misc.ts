b579a4c7595ac2bcba8675d5dbf31a1891e1ec9e
ByungJoon Lee <imjuni@gmail.com>
2021-03-11 16:18:09 +0900

    Add new function typedkey
    * Add new function typedkey
        * typedkey return array of object: type matched array
        * If you want number and symbol key, pass convertor

---
+++ src/misc.ts
@@ -0,0 +1,8 @@
+export function typedkey<KT extends string | number | symbol, VT, O extends Record<KT, VT>>(
+  value: O,
+  convertor?: (k: string) => keyof O,
+): (keyof O)[] {
+  return convertor !== undefined && convertor !== null
+    ? (Object.keys(value) as any).map((key: any) => convertor(key))
+    : (Object.keys(value) as any);
+}
