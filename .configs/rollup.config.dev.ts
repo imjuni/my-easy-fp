7869ca5cd77441c7594a97a8a44aa2ad8f9c8021
ByungJoon Lee <imjuni@gmail.com>
2023-01-10 04:35:29 +0900

    Fix audit
    * version upgrade 0.18.0 > 0.18.1
    * deps
      * dev
        * add @rollup/plugin-terser: 0.3.0
        * remove rollup-plugin-sourcemaps: 0.6.3
        * remove rollup-plugin-terser: 7.0.2

--- .configs/rollup.config.dev.ts
+++ .configs/rollup.config.dev.ts
@@ -1,7 +1,7 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import readPackage from 'read-pkg';
 import ts from 'rollup-plugin-ts';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 const pkg = readPackage.sync();
 
