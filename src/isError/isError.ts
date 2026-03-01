b5a6d5513f3b06b1126edf0bd7b6c34310c409e2
ByungJoon Lee <imjuni@gmail.com>
2026-03-02 02:17:57 +0900

    feat(isError): enhance type safety with generic error handling

--- src/isError/isError.ts
+++ src/isError/isError.ts
@@ -1,18 +1,19 @@
-function isError(err: unknown): Error | undefined;
-function isError(err: unknown, defaultValue: Error): Error;
-function isError(err?: unknown, defaultValue?: Error): Error | undefined {
+function isError<E extends Error = Error>(err: unknown): err is E;
+function isError<E extends Error = Error>(err: unknown, defaultValue: E): E;
+function isError<E extends Error = Error>(err?: unknown, defaultValue?: E): E | undefined {
   if (err instanceof Error) {
-    return err;
+    return err as E;
   }
 
   if (
     err !== undefined &&
     err !== null &&
     typeof err === 'object' &&
+    'name' in err &&
     'message' in err &&
     'stack' in err
   ) {
-    return err as Error;
+    return err as E;
   }
 
   if (defaultValue != null) {
